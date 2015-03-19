832e0e93eab604cd40d36b66cdf6d6046421572a
kenjiheigel <kenji.heigel@liferay.com>
2015-03-19 15:49:27 -0700

    LRQA-14452 - Clean up unused mobile properties from the tests

--- modules/test/poshi-runner/src/com/liferay/poshi/runner/selenium/FirefoxWebDriverImpl.java
+++ modules/test/poshi-runner/src/com/liferay/poshi/runner/selenium/FirefoxWebDriverImpl.java
@@ -72,11 +72,6 @@ public class FirefoxWebDriverImpl extends BaseWebDriverImpl {
 		_firefoxProfile.setPreference("dom.max_chrome_script_run_time", 300);
 		_firefoxProfile.setPreference("dom.max_script_run_time", 300);
 
-		if (PropsValues.MOBILE_DEVICE_ENABLED) {
-			_firefoxProfile.setPreference(
-				"general.useragent.override",
-				PropsValues.MOBILE_DEVICE_USER_AGENT);
-		}
 	}
 
 }
