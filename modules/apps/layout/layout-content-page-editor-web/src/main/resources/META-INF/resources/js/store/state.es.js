5c5ca47350aec8ad98ca8502131dc6cf2150c88a
Sandro Chinea <sandrochinea@gmail.com>
2019-03-19 10:48:54 +0100

    LPS-92236 URL for getting an asset field value added to state and editables component

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/store/state.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/store/state.es.js
@@ -356,6 +356,16 @@ const INITIAL_STATE = {
 		.value(''),
 
 	/**
+	 * URL for getting an asset field value
+	 * @default ''
+	 * @review
+	 * @type {string}
+	 */
+	getAssetFieldValueURL: Config
+		.string()
+		.value(''),
+
+	/**
 	 * Get asset mapping fields url
 	 * @default undefined
 	 * @review
