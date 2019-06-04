36dd5d66cf4bdbede7e48e34d6c1c2efbb39f810
Brian Chiu <brian.chiu@liferay.com>
2019-06-04 16:15:33 -0700

    Revert "LPS-95728 Includes bootstrap.js and popper.js from the jquery module so they can be disabled using the jquery configuration option"
    This reverts commit 79bea39606b29b6bc9d05ca84756955ebc430b44.

--- modules/apps/frontend-js/frontend-js-jquery-web/src/main/java/com/liferay/frontend/js/jquery/web/internal/servlet/taglib/JQueryTopHeadDynamicInclude.java
+++ modules/apps/frontend-js/frontend-js-jquery-web/src/main/java/com/liferay/frontend/js/jquery/web/internal/servlet/taglib/JQueryTopHeadDynamicInclude.java
@@ -123,9 +123,8 @@ public class JQueryTopHeadDynamicInclude extends BaseDynamicInclude {
 	}
 
 	private static final String[] _FILE_NAMES = {
-		"/jquery/jquery.min.js", "/jquery/bootstrap.bundle.min.js",
-		"/jquery/collapsible-search.js", "/jquery/fm.js", "/jquery/form.js",
-		"/jquery/popper.min.js", "/jquery/side-navigation.js"
+		"/jquery/jquery.js", "/jquery/collapsible-search.js", "/jquery/fm.js",
+		"/jquery/form.js", "/jquery/side-navigation.js"
 	};
 
 	@Reference
